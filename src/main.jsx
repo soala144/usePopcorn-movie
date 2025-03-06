502aa12f7ac72e47c6c9b63cab79843c6b255658
soala144 <soala144@gmail.com>
2025-03-06 03:30:36 -0800

    Reusabilty to configure with props

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
+// import { useState } from "react";
 import StarRating from "./StarRating";
 // import './index.css'
 // import App from './App.jsx'
@@ -7,8 +8,22 @@ import StarRating from "./StarRating";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    {/* <StarRating maxRating={10} />
-    <StarRating /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Good", "Okay", "Awesome", "Fantastic"]}
+    />
+    <StarRating maxRating={6} color="red" defaultRating={3} />
+    <Test />
+    {/* <StarRating /> */}
   </StrictMode>
 );
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating}</p>
+    </div>
+  );
+}
