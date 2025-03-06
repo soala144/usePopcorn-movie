f27a57c4e9735fd3972eb6a46897be17320211b0
soala144 <soala144@gmail.com>
2025-03-05 20:13:33 -0800

    Initial Star

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
 
-const StarRating = () => {
-  return <div>StarRating</div>;
+const StarContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+const StarRating = ({ maxRating = 5 }) => {
+  return (
+    <div style={containerStyle}>
+      <div style={StarContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p>{maxRating}</p>
+    </div>
+  );
 };
 
 export default StarRating;
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import StarRating from "./StarRating";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
     <StarRating />
   </StrictMode>
 );
